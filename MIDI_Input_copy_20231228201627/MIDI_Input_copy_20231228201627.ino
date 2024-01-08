b12cedf59f9a5d51b3eaa8929789c159f5f7ece9
happyaardbei <155104548+happyaardbei@users.noreply.github.com>
2024-01-08 20:59:34 +0100

    yeah

--- MIDI_Input_copy_20231228201627/MIDI_Input_copy_20231228201627.ino
+++ MIDI_Input_copy_20231228201627/MIDI_Input_copy_20231228201627.ino
@@ -4,9 +4,11 @@
 
 //Create an instance of the library with default name, serial port and settings
 MIDI_CREATE_DEFAULT_INSTANCE();
+  bool status = false;
+
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(750);
   pinMode (LED, OUTPUT); // Set Arduino board pin 13 to output
   MIDI.begin(MIDI_CHANNEL_OMNI); // Initialize the Midi Library.
   // OMNI sets it to listen to all channels.. MIDI.begin(2) would set it 
@@ -16,25 +18,36 @@ void setup() {
   // is received. In this case it's "MyHandleNoteOn".
   MIDI.setHandleNoteOff(MyHandleNoteOff); // This command tells the Midi Library 
   // to call "MyHandleNoteOff" when a NOTE OFF command is received.
-  MIDI.setHandleControlChange(flicker);
+  MIDI.setHandleControlChange(ControlChange);
   //MIDI.setHandlePitchBend(flicker);
 }
 
 void loop() { // Main loop
-  //Serial.println("moi kerel");
-  //Serial.print("\n");
-  //delay(1000);
+  // Serial.println("moi kerel");
+  // Serial.print("\n");
+  // delay(1000);
+  if(status == true ) {
+    flicker(1);
+  }
   MIDI.read(); // Continuously check if Midi data has been received.
 }
 
 // MyHandleNoteON is the function that will be called by the Midi Library
 // when a MIDI NOTE ON message is received.
 // It will be passed bytes for Channel, Pitch, and Velocity
-void MyHandleNoteOn(byte channel, byte note, byte pitch, byte velocity) { 
-  
-    digitalWrite(LED,true);  //Turn LED 
-  
-  
+void MyHandleNoteOn(byte channel, byte number, byte pitch, byte velocity) { 
+  digitalWrite(LED, true);  
+}
+
+void ControlChange(byte channel, byte number, byte pitch, byte velocity)  {
+
+  if(number == 1 && status == false) {
+    digitalWrite(LED, true);
+    status = true;
+  } else {
+    digitalWrite(LED, false);
+    status = false;
+  }
 }
 
 // MyHandleNoteOFF is the function that will be called by the Midi Library
@@ -46,8 +59,8 @@ void MyHandleNoteOff(byte channel, byte note, byte pitch, byte velocity) {
   
 }
 
-void flicker() {
-  for(int i = 0; i < 10; i++) {
+void flicker(byte count) {
+  for(int i = 0; i < count; i++) {
     digitalWrite(LED,true);
     delay(40);
     digitalWrite(LED,false);
